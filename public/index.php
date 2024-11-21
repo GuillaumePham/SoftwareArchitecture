6a121cf6fab2f1f215d66a422b376f793cf71b09
pham <pham.guillome@gmail.com>
2024-11-21 12:21:14 +0100

    Adiing Calcul Priorities

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
 use App\Calculator\CalculatorApp;
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new CalculatorApp();
 $args = $argv;
 array_shift($args);
 $app($args);
+
--- src/Calculator.php
+++ src/Calculator.php
@@ -23,21 +23,80 @@ class Calculator
         ];
     }
 
-    public function calculate(array $args): float
+    private function getOperatorOrderNumber(string $operator): int
     {
-        $result = (float)array_shift($args);
+        $OrderNumber = [
+            '+' => 1,
+            '-' => 1,
+            '*' => 2,
+            '/' => 2,
+            'cos' => 3,
+        ];
 
-        while (count($args) > 1) {
-            $operator = array_shift($args);
-            $number = (float)array_shift($args);
-
-            if (!isset($this->operations[$operator])) {
-                throw new \Exception("Opérateur invalide : $operator");
-            }
-
-            $result = $this->operations[$operator]->calculate($result, $number);
+        if (!array_key_exists($operator, $OrderNumber)) {
+            throw new \Exception("Opérateur non reconnu : $operator");
         }
 
-        return $result;
+        return $OrderNumber[$operator];
+    }
+
+    private function toPostfix(array $args): array //
+    {
+        $output = [];
+        $stack = [];
+
+        foreach ($args as $arg) {
+            if (is_numeric($arg)) {
+                $output[] = $arg;
+            } elseif ($arg === '(') {
+                $stack[] = $arg;
+            } elseif ($arg === ')') {
+                while (!empty($stack) && end($stack) !== '(') {
+                    $output[] = array_pop($stack);
+                }
+                array_pop($stack);
+            } elseif (isset($this->operations[$arg])) {
+                while (!empty($stack) && $this->getOperatorOrderNumber(end($stack)) >= $this->getOperatorOrderNumber($arg)) {
+                    $output[] = array_pop($stack);
+                }
+                $stack[] = $arg;
+            } else {
+                throw new \Exception("Argument invalide : $arg");
+            }
+        }
+
+        while (!empty($stack)) {
+            $output[] = array_pop($stack);
+        }
+
+        return $output;
+    }
+
+    private function evaluatePostfix(array $postfix): float
+    {
+        $stack = [];
+
+        foreach ($postfix as $token) {
+            if (is_numeric($token)) {
+                $stack[] = (float)$token;
+            } elseif (isset($this->operations[$token])) {
+                $b = array_pop($stack);
+                $a = array_pop($stack);
+                $result = $this->operations[$token]->calculate($a, $b);
+                $stack[] = $result;
+            } else {
+                throw new \Exception("Token invalide : $token");
+            }
+        }
+
+        return array_pop($stack);
+    }
+
+    public function calculate(array $args): float
+    {
+        $postfix = $this->toPostfix($args);
+        return $this->evaluatePostfix($postfix);
     }
 }
+
+
